4cf8fd50a36763fa3c53a7337d0ecceb7d98507b
addy1234 <aditya123garg@gmail.com>
2020-08-25 04:46:48 +0530

    added event listener and keypressor for submit button

--- app.js
+++ app.js
@@ -3,27 +3,53 @@ var budgetController = (function(){
 
 })();
 
-var UIController = (function(){
+var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputSubmitButton: '.add__btn'
+    };
+
+    return {
+        getinput: function() {
+            return {
+                // Will be either inc or dec.
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 
 })();
 
 
 var controller = (function(budgetCtrl, UICtrl){
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
+    var DOM = UICtrl.getDOMstrings();
+    console.log(DOM);
+    var ctrlAddItem = function() {
+        // Get the input field.
+        var input = UICtrl.getinput();
+        console.log(input);
+    }
 
-    });
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
 
         // old browsers use 'which' instead of 'KeyCode' property.
         if (event.keyCode === 13 || event.which === 13) {
-            console.log("Enter Key");
-        } else {
-            console.log("Another Key");
-        }
-        console.log(event);
+            //console.log("Enter Key");
+            ctrlAddItem();
+        } 
+        //console.log(event);
     });
 
 })(budgetController, UIController);
