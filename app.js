d23ec2bb780d6d7a01d8f16a43f4f3c3930daa9c
addy1234 <aditya123garg@gmail.com>
2020-08-28 23:16:36 +0530

    budget controller functions added

--- app.js
+++ app.js
@@ -32,24 +32,35 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl){
 
-    var DOM = UICtrl.getDOMstrings();
-    console.log(DOM);
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputSubmitButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+    
+            // old browsers use 'which' instead of 'KeyCode' property.
+            if (event.keyCode === 13 || event.which === 13) {
+                //console.log("Enter Key");
+                ctrlAddItem();
+            } 
+            //console.log(event);
+        });    
+    }
+
     var ctrlAddItem = function() {
         // Get the input field.
         var input = UICtrl.getinput();
         console.log(input);
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-
-        // old browsers use 'which' instead of 'KeyCode' property.
-        if (event.keyCode === 13 || event.which === 13) {
-            //console.log("Enter Key");
-            ctrlAddItem();
-        } 
-        //console.log(event);
-    });
+    return {
+        init: function() {
+            console.log("Init called!");
+            setupEventListeners();
+        }
+    }
 
 })(budgetController, UIController);
+
+controller.init();
